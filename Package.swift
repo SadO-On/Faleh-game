9e1f54697cbf43677b0788d9054dd3429139190b
Mohammed Waleed <mr.sadoun@gmail.com>
2024-02-26 04:44:14 +0300

    zq

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Faleh-game-package",
+            type: .static,
             targets: ["Faleh-game-package"]),
     ],
     dependencies: [
